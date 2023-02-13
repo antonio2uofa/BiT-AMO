b6602a30a2b97a3c16c1fa7590295f08002cea7b
Antonio Martin-O <90714463+antonio2uofa@users.noreply.github.com>
2023-02-13 08:40:10 -0700

    Non OPC to be ignored
    Removed Non OPC directory so as to avoid further confusion

--- Non OPC/Wrapper Code/closeUsb.py
+++
@@ -1,6 +0,0 @@
-from pywrapper import *
-
-def closeUsb():
-	return(c_lib.Balls_usb_close())
-
-def 
--- Non OPC/Wrapper Code/unloadbit.py
+++
@@ -1,7 +0,0 @@
-from pywrapper import *
-
-# Create a handle for the c library
-handle = c_lib._handle
-
-#Unload the DLL file
-ctypes.windll.kernel32.FreeLibrary(handle)
