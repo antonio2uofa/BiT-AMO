49e026584526de7dd2acede62c5326ff064f1012
Nash <nggould@ualberta.ca>
2023-02-28 12:50:40 -0700

    Changed band range for frequency

--- Data/generate_rgs.m
+++ Data/generate_rgs.m
@@ -4,16 +4,17 @@ N = 480;
 Nu = 1;
 ch = 1;
 Type = 'rgs';
-Band = [0.01, 0.27];
+Band1 = [0.01, 0.27];
+Band2 = [0.01, 0.54];
 Range4 = [52 58];
-Range3 = [48 60];
+Range3 = [55 65];
 Range2 = [55 63];
 Range1 = [55 65];
 
-U4 = idinput([N, ch, Nu], Type, Band, Range4);
-U3 = idinput([N, ch, Nu], Type, Band, Range3);
-U2 = idinput([N, ch, Nu], Type, Band, Range2);
-U1 = idinput([N, ch, Nu], Type, Band, Range1);
+U4 = idinput([N, ch, Nu], Type, Band1, Range4);
+U3 = idinput([N, ch, Nu], Type, Band2, Range3);
+U2 = idinput([N, ch, Nu], Type, Band1, Range2);
+U1 = idinput([N, ch, Nu], Type, Band1, Range1);
 
 U4 = cast(U4, 'int16');
 U3 = cast(U3, 'int16');
@@ -27,3 +28,5 @@ step_table.Properties.VariableNames(1:4) = {
     };
 
 writetable(step_table, 'rgs_signals.csv');
+
+clc;
