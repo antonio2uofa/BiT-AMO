611309c7453923adfab55a4fd28bd871e59a55c6
Antonio Martin-O <antonio2@ualberta.ca>
2023-04-05 12:17:50 -0600

    Added deleted line back

--- BiT_functions.py
+++ BiT_functions.py
@@ -51,6 +51,19 @@ def set_fanspeed(tube_num, level):
 	level = str(level)
 	run(executable, input=level, text=True)
 
+def set_fanspeeds(fan_4, fan_3, fan_2, fan_1, normalized=False):
+	fanspeeds = [fan_4, fan_3, fan_2, fan_1]
+	executable = "./cfiles/set_fanspeeds"
+	max_width = 4095
+	if(normalized):
+		scaled_vars = [round(speed * max_width) for speed in fanspeeds]
+	else:
+		scaled_vars = [round((speed / 100) * max_width) for speed in fanspeeds]
+	scaled_vars = [str(speed) for speed in scaled_vars]
+	speeds = ' '.join(scaled_vars)
+	byte_speeds = speeds.encode('utf-8')
+	run(executable, input=byte_speeds)
+
 #------------------------------------------------------------------------#
 #	Function Used to Get Level of the Ball in a Specified Tube			 #
 #------------------------------------------------------------------------#
